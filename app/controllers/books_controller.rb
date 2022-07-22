c7e786e7ff9087a5b37833152a0ba46d462fe7f0
EC2 Default User <ec2-user@ip-172-31-43-203.ec2.internal>
2022-07-22 16:07:55 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   def new
     @books = Book.new
   end
@@ -11,24 +11,29 @@ class BooksController < ApplicationController
     @book.save
     redirect_to books_path
   end
-  
-  
+
+  #indexアクションは一覧画面ようのアクションとして定義します。 
+  #一覧画面には投稿したBook全てを表示させるので、全てのデータを取得する記述をします。
+  #Controllerのアクションでインスタンス変数を指定しておくことで、
+  #viewファイル上でインスタンス変数に格納された情報を表示させることができます。
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:])
   end
-  
+
   def destroy
   end
 
-  
+
   # 投稿データのストロングパラメータ
   private
 
   def book_params
-    params.require(:book).permit(:book_name, :image :caption)
+    params.require(:book).permit(:book_name, :image, :caption)
   end
-  
-  
+
+
 end
